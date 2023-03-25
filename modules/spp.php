8889436bff29c4a7f0cd7248099d016bc480f002
samukacf <112741334+samukacf@users.noreply.github.com>
2023-03-25 18:41:20 +0000

    Update spp.php

--- modules/spp.php
+++ modules/spp.php
@@ -17,7 +17,7 @@ $cvv = multiexplode(array(":", "/", " ", "|"), $message)[3];
       $mes_id = $sent['result']['message_id'];
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'http://20.212.107.64/sendnudez/Hcaptcha/chk.php?lista='.$lista.'');
+curl_setopt($ch, CURLOPT_URL, 'https://smk.herokuapp.com/modules/spp.php?lista='.$lista.'');
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $fimaa = curl_exec($ch);
